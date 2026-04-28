f3f2373cddf9234eaac6b84fb5c2cd714acfbff2
fer4rie <firapuspita2106@gmail.com>
2026-04-28 15:30:58 +0700

    add pointer reference to mahasiswa object and update nim to 2

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -11,3 +11,7 @@ class mahasiswa{
 int main(){
     mahasiswa mhs{1}; //object mhs
     mhs.showNim(); //Member Access Operator
+
+    mahasiswa &refMhs = mhs; //Pointer Reference refMhs
+    refMhs.nim = 2; //Member Access Operator
+    mhs.showNim();
